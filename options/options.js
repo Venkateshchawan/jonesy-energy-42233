f1fbb01f264cc8947d997100f65175934e4b0453
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-07-25 16:37:05 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "polished-truth-42233",
-  url: "https://polished-truth-42233.botics.co",
-  api: "https://polished-truth-42233.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "polished-truth-42233",
+  "url": "https://polished-truth-42233.botics.co",
+  "api": "https://polished-truth-42233.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI10"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
