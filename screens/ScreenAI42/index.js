cecf1f9f6ab3989bc78a23815d914111e0b695f3
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-08-25 14:49:21 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI42/index.js
+++ screens/ScreenAI42/index.js
@@ -1,7 +1,10 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 
 const Screen = () => {
+  const navigation = useNavigation();
   const [keyword, setKeyword] = useState('');
   const [clients, setClients] = useState([{
     id: 1,
@@ -41,7 +44,11 @@ const Screen = () => {
         <TextInput style={styles.searchBar} placeholder="Search" value={keyword} onChangeText={text => setKeyword(text)} />
       </View>
       <FlatList data={filteredClients} renderItem={renderClientItem} keyExtractor={item => item.id.toString()} />
-    </SafeAreaView>;
+    <Pressable onPress={() => {
+      navigation.navigate("ScreenAI40");
+    }}><Text style={styles.nBaNVifw}>{"1"}</Text></Pressable><Pressable onPress={() => {
+      navigation.navigate("ScreenAI41");
+    }}><Text style={styles.TKxNLrGG}>{"2"}</Text></Pressable></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
@@ -79,6 +86,20 @@ const styles = StyleSheet.create({
   email: {
     fontSize: 16,
     marginBottom: 8
+  },
+  nBaNVifw: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 5,
+    borderRadius: 0
+  },
+  TKxNLrGG: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 5,
+    borderRadius: 0
   }
 });
 export default Screen;
